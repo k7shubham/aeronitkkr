3aaf517115c8e12d4ec93f26aa2481a050b59053
Amit <amit99kumar4826@gmail.com>
2020-05-16 18:33:25 +0530

    added preloader

--- js/init.js
+++ js/init.js
@@ -6,3 +6,7 @@
     $('.materialboxed').materialbox();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(document).ready(function() {
+	$('.sk-folding-cube-wrapper').fadeOut(500);
+});
